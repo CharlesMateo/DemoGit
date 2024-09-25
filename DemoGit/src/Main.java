87bf1177a7cad677f973abd2c6a055e62ae2a6b8
Moon Knight <Charlesmateo18@gmail.com>
2024-09-25 14:01:25 -0700

    Creating demo variables for git

---
+++ DemoGit/src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome home Moon Knight... ");
+
+        String CodingIs = "Actually cool.";
+
+        System.out.println("Coding is " + CodingIs);
+    }
+}
